d556353549077abe62ce0ed9ab7e1d0206b4901f
tarun sharma <tarunsharma8920@gmail.com>
2019-05-31 23:57:59 +0530

    Added some new lines of code.

--- fun.py
+++ fun.py
@@ -2,6 +2,8 @@
 
 print("Hello World!!")
 a = input("What is your name ?")
+print("It is good to meet you, " + a)
 b =int(input("what is your current age ?"))
-
+print("The length of your name is:")
+print(len(a))
 print( a , "you will be " + str(((b) + 1)) + " years old in a year!!")
