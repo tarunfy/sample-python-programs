e65a76bb54f0492651a17f73a254d18146b1b5bb
tarun sharma <tarunsharma8920@gmail.com>
2019-05-31 23:34:49 +0530

    Funny simple program, it will ask your age and name the gives a crazy statement .

---
+++ fun.py
@@ -0,0 +1,7 @@
+#This program ask for my name and says hello.
+
+print("Hello World!!")
+a = input("What is your name ?")
+b =int(input("what is your current age ?"))
+
+print( a , "you will be " + str(((b) + 1)) + " years old in a year!!")
