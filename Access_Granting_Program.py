563147e3d0cc4d0926d5b30b38c43d28a2ca1558
tarun sharma <tarunsharma8920@gmail.com>
2019-06-02 16:16:14 +0530

    Sneek peek of if else program.

---
+++ Access_Granting_Program.py
@@ -0,0 +1,10 @@
+User_Name = 'Tarun'
+Password = 'I Dont Know'
+if User_Name == 'Tarun':
+    print("Hi Tarun, Welcome back!")
+    if Password == "I Dont Know":
+        print("Access Granted")
+    else:
+        print("Wrong Password.")
+
+
