b100297404c3aa5347301359f23b060127323aa5
tarun sharma <tarunsharma8920@gmail.com>
2019-06-05 17:07:29 +0530

    cool

---
+++ loop ask password and name.py
@@ -0,0 +1,15 @@
+while True:
+    print("What is your name?")
+    name = input()
+    if name != "Tarun":
+        print("Try Again.")
+        continue
+    print("hi Tarun, welcome back")
+    print("what is the password?")
+    password = input()
+    if password == "lol":
+        break
+print("Access Granted >3")
+
+
+
