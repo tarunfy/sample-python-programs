14916dd06efd7341c31044ec5c1e2a534261f4a9
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-10-17 15:39:21 +0530

    using grid to add widgets anywhere exactly

--- Tkinter_1.py
+++ Tkinter_1.py
@@ -1,21 +1,53 @@
 from tkinter import *
 
 root = Tk()
+# Simple gui with a text:
 
-topFrame = Frame(root)
-topFrame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+# label1 = Label(root, text="Hello World")
+# label1.pack()
 
-button1 = Button(topFrame, text="Button 1", fg="cyan")
-button2 = Button(topFrame, text="Button 2", fg="red")
-button3 = Button(topFrame, text="Button 3", fg="purple")
-button4 = Button(bottomFrame, text="Button 4", fg="green")
+# ADDING BUTTONS:
 
-button1.pack(side=LEFT)
-button2.pack(side=LEFT)
-button3.pack(side=LEFT)
-button4.pack()
+# topFrame = Frame(root)
+# topFrame.pack()
+# bottomFrame = Frame(root)
+# bottomFrame.pack(side=BOTTOM)
+
+# button1 = Button(topFrame, text="Button 1", fg="cyan")
+# button2 = Button(topFrame, text="Button 2", fg="red")
+# button3 = Button(topFrame, text="Button 3", fg="purple")
+# button4 = Button(bottomFrame, text="Button 4", fg="green")
+
+# button1.pack(side=LEFT)
+# button2.pack(side=LEFT)
+# button3.pack(side=LEFT)
+# button4.pack()
+
+# ADDING WIDGETS:
+
+# one = Label(root, text= "One", bg="red", fg="green")
+# one.pack()
+
+# two = Label(root, text= "Two", bg="blue", fg="yellow")
+# two.pack(fill=X)
+
+# three = Label(root, text= "Three", bg="green", fg="black")
+# three.pack(side=LEFT, fill=Y)
+
+label1 = Label(root, text="Name")
+label2 = Label(root, text="Password")
+entry_1 = Entry(root)
+entry_2 = Entry(root)
+
+label1.grid(row=0, sticky=E)
+label2.grid(row=1, sticky=E)
+
+entry_1.grid(row=0, column=1)
+entry_2.grid(row=1, column=1)
+
+c = Checkbutton(root, text="Keep me logged in")
+c.grid(columnspan=2)
+
 
 root.mainloop()
 
