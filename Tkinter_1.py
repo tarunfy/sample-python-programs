e2bea66105bd177e83757851a366d8e08978a255
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-10-17 15:53:49 +0530

    added buttons and their commands

--- Tkinter_1.py
+++ Tkinter_1.py
@@ -2,12 +2,10 @@ from tkinter import *
 
 root = Tk()
 # Simple gui with a text:
-
 # label1 = Label(root, text="Hello World")
 # label1.pack()
 
 # ADDING BUTTONS:
-
 # topFrame = Frame(root)
 # topFrame.pack()
 # bottomFrame = Frame(root)
@@ -24,7 +22,6 @@ root = Tk()
 # button4.pack()
 
 # ADDING WIDGETS:
-
 # one = Label(root, text= "One", bg="red", fg="green")
 # one.pack()
 
@@ -34,20 +31,33 @@ root = Tk()
 # three = Label(root, text= "Three", bg="green", fg="black")
 # three.pack(side=LEFT, fill=Y)
 
-label1 = Label(root, text="Name")
-label2 = Label(root, text="Password")
-entry_1 = Entry(root)
-entry_2 = Entry(root)
+# ADDING WIDGETS USING GRID:
+# label1 = Label(root, text="Name")
+# label2 = Label(root, text="Password")
+# entry_1 = Entry(root)
+# entry_2 = Entry(root)
 
-label1.grid(row=0, sticky=E)
-label2.grid(row=1, sticky=E)
+# label1.grid(row=0, sticky=E)
+# label2.grid(row=1, sticky=E)
 
-entry_1.grid(row=0, column=1)
-entry_2.grid(row=1, column=1)
+# entry_1.grid(row=0, column=1)
+# entry_2.grid(row=1, column=1)
 
-c = Checkbutton(root, text="Keep me logged in")
-c.grid(columnspan=2)
+# c = Checkbutton(root, text="Keep me logged in")
+# c.grid(columnspan=2)
 
+# Calling a function when a button is clicked:
+# Trick 1:
+def printName():
+    print("Hello my name is Tarun")
+button_1 = Button(root, text= "Print my name", command=printName)
+button_1.pack()
 
+# Trick 2:
+# def printName(event): 
+#     print("Hello my name is Tarun")
+# button_1 = Button(root, text= "Print my name")
+# button_1.bind("<Button-1>", printName)
+# button_1.pack()
 root.mainloop()
 
