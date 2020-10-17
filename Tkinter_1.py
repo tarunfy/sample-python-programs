2247f8127e95c6f09645bdefc11195708c12b3a4
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-10-17 15:12:31 +0530

    added buttons to the gui

--- Tkinter_1.py
+++ Tkinter_1.py
@@ -1,7 +1,21 @@
 from tkinter import *
 
 root = Tk()
-label1 = Label(root, text = "Yooo sup bro")
-label1.pack()
+
+topFrame = Frame(root)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text="Button 1", fg="cyan")
+button2 = Button(topFrame, text="Button 2", fg="red")
+button3 = Button(topFrame, text="Button 3", fg="purple")
+button4 = Button(bottomFrame, text="Button 4", fg="green")
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack()
+
 root.mainloop()
 
