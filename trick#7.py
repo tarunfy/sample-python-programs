452b5024f978f3d8184480c9e2d7c5a65e1ae396
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-11-11 22:07:49 +0530

    trick

---
+++ trick#7.py
@@ -0,0 +1,9 @@
+# Alerts while taking input of users's sensitive infos
+
+from getpass import getpass
+
+username = input("Username: ")
+password = getpass("Password: ")
+
+print("Logging in.....")
+
