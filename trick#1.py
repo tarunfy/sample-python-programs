4d7aea8f2b918aee5ca1a8b96bd32945ee2fa538
Tarun Sharma <51208270+tarun2506@users.noreply.github.com>
2020-11-11 22:05:54 +0530

    trick

---
+++ trick#1.py
@@ -0,0 +1,14 @@
+condition = True
+
+# Ternary conditional(ek line mai he condition dedi)
+
+x = 1 if condition else 0 
+
+#Normal way of writing conditions:
+
+# if condition:
+#     x=1
+# else:
+#     x=0
+
+print(x)
